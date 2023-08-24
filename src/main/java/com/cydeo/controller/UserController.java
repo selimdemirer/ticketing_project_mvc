632385be2c4bc97a31d31d13fedf2aa462106a16
selimdemirer <selimmdemirerr@gmail.com>
2023-08-24 13:52:20 +0300

    delete button is done, user page completed

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -65,4 +65,12 @@ public class UserController {
         return "redirect:/user/create";
     }
 
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
 }
