83c53858b23e605a1efde3a635cd9509a8172bf1
selimdemirer <selimmdemirerr@gmail.com>
2023-08-25 13:43:36 +0300

    Task Create page started

---
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,47 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
+    @Override
+    public TaskDTO save(TaskDTO task) {
+
+        if(task.getTaskStatus()==null){
+            task.setTaskStatus(Status.OPEN);
+        }
+
+        return super.save(task.getId(), task);
+    }
+
+    @Override
+    public TaskDTO findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<TaskDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void update(TaskDTO task) {
+
+        if(task.getTaskStatus()==null){
+            task.setTaskStatus(findById(task.getId()).getTaskStatus());
+        }
+
+        super.update(task.getId(), task);
+
+    }
+}
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,11 +1,8 @@
 package com.cydeo.service.impl;
 
-import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
