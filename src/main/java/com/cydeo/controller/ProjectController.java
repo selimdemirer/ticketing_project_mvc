1bbdec36bea4933333af5e1cfcf568b752d2fc26
selimdemirer <selimmdemirerr@gmail.com>
2024-07-05 05:38:30 +0300

    Ticketing Project (Project Management Tool) - MVC review is continuing

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -34,7 +34,7 @@ public class ProjectController {
     }
 
     @PostMapping("/create")
-    public String insertProject(@ModelAttribute("project") ProjectDTO project, Model model) {
+    public String insertProject(@ModelAttribute("project") ProjectDTO project) {
 
         projectService.save(project);
 
